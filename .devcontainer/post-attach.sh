50f50de63e4d0db64bfdb32477aaf9f2238f1456
wtfzambo <47462792+wtfzambo@users.noreply.github.com>
2023-04-13 08:11:05 -0400

    Update post-attach.sh
    Run spark history server after attach.

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -3,3 +3,5 @@
 set -ex
 
 git config --list | grep "safe.directory=$(pwd)" || git config --global --add safe.directory $(pwd)
+
+~/spark/sbin/start-history-server.sh || echo "Spark history server already running!"
